601ca7aca2fed65f170012c1a393f0fa504962b7
Pankaj Goyal <pankaj.pankaj890@gmail.com>
2020-04-10 11:32:39 +0530

    Initial

--- venv/App/MultiBrowser.py
+++ venv/App/MultiBrowser.py
@@ -3,10 +3,12 @@ from selenium.webdriver.common.keys import Keys
 
 # Create Driver Object
 driver = webdriver.Chrome(executable_path="C:\Drivers\chromedriver.exe")  # Chrome
-driver = webdriver.Chrome(executable_path="C:\Drivers\geckodriver.exe")   # Firefox
+#driver = webdriver.Chrome(executable_path="C:\Drivers\geckodriver.exe")   # Firefox
 
 driver.get("http://newtours.demoaut.com/")
 
-print(driver.title)
+print(driver.title)     # Title of the Webpage
+print(driver.current_url)     # URL of the Webpage
+print(driver.page_source)     # HTML of the Webpage
 
 driver.close()       # Close the browser
